d7083b8f5eca8addbb21d1391e08cf6f1ac81a00
LEVITRE Mathys <levitre.mathys30@gmail.com>
2025-05-21 13:48:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,19 +8,17 @@ $(document).ready(function() {
             $(".btnDarkSide").html("<img src='./assets/img/mobile-apps-website-screen-brightness-260nw-2283103921(1).png' alt='Dark mode toggle'>")
         }
     })
-
-     $(".hamburger").click(function() {
-        $(".nav_bar1, .nav_bar2").toggleClass("active");
-    });
-
-    $(".myImage").click(function() {
+    
+    $(".voirCV").click(function(event) {
+        event.preventDefault();
+        $(".lightbox").html("<img src= '" + $(".voirCV").attr("href") + "'>");
         $(".voile").fadeIn();
         $(".lightbox").fadeIn();
     })
+    
 
     $(".voile").click(function() {
         $(".voile").fadeOut();
         $(".lightbox").fadeOut();
     })
-
 })
